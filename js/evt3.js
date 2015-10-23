fdc3aace5a4131fe7dead183083887135bfdc8a3
nongs <fovostra@gmail.com>
2015-10-23 12:30:14 +0900

    Update evt3.js

--- js/evt3.js
+++ js/evt3.js
@@ -25,4 +25,31 @@ $(function() {
 		$("#bgbt").fadeOut();
 		//$(".close").fadeOut();
 	});
+});$(function() {
+    $( "#releas" ).on("tap",function(){
+		$("nav").animate({bottom: "0"});
+		$("#releas").hide();
+		$("#releas1").show();
+	});
+});
+$(function() {
+    $( "#releas1" ).on("tap",function(){
+		$("nav").animate({bottom: "-200px"});
+		$("#releas1").hide();
+		$("#releas").show();
+	});
+});
+$(function() {
+    $( "#entered" ).on("tap",function(){
+		$("#about").fadeIn();
+		$("#bgbt").fadeIn();
+		//$(".close").fadeIn();
+	});
+});
+$(function() {
+    $( ".close,#bgbt" ).on("tap",function(){
+		$("#about").fadeOut();
+		$("#bgbt").fadeOut();
+		//$(".close").fadeOut();
+	});
 });
