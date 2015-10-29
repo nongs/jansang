6e9be963a1cbbe85df6c36bc4e711bda1b98e8ba
nongs <fovostra@gmail.com>
2015-10-30 06:18:42 +0900

    act3
    act3

--- js/evt13.js
+++ js/evt13.js
@@ -4,7 +4,7 @@ function nextRandomInteger(limit) {
 }
 
 //랜덤한 문자를 리턴하는 함수
-var letters = ["입에서 시궁창 몸에서 썩은내", "그냥 폐에다가 꽂아라 쓰레기들아", "노노 꼬운게 아니라 역겨운거", "병자들이 돌아댕기네", "그래서 사회생활은 어떻게 하냐? 머저리들", "한방에 벌레일망타진", "기분 개같이 더럽네", "암이나 걸려 뒈져 쓰글넘들", "만나면똥지릴 놈들", "대가리에 든거 없는 것들이 왜 이렇게 많은지", "미개한건 어디나 다 미개하네"];
+var letters = ["이건뭐 반려견들도 아니고", "수준 떨어진다는 생각은 전혀 못하는 좌빨 또라이", "뭔 아가리질이냐 버러지만도 못한 병신은", "안때려죽인걸 감사나해라 상놈의 새끼야!", "한심한 새끼 쯧", "진짜 역겹다", "지랄들해라아주", "과대망상 정신병자 집단인가", "국민의 탈을쓰고있는 멍청한 노예 호구새끼들이 더 깝깝하다", "너는 벌써 디졌어 상년아", "완전히 미쳐가지고 개꼴깝질이나 쳐하고 자빠진년이"];
 function randomLetter() {
     letterIndex = Math.floor(Math.random() * letters.length);
     letterToGuess = letters[letterIndex];
@@ -18,7 +18,7 @@ function randomSize() {
 
 //양음으로 랜덤한 속도를 생성하는 함수
 function randomSpeed(maxSpeed) {
-    return Math.floor(Math.random() * 5 + 3) * maxSpeed;
+    return Math.floor(Math.random() * 3 +1) * maxSpeed;
 }
 
 //var canvas = document.getElementById('myCanvas');
@@ -41,10 +41,10 @@ var canvasHeight = H;
 
 function MovingText() {
     //위치와 속도 생성
-    this.x = W;
-    this.y = nextRandomInteger(canvasHeight);
-    this.vx = randomSpeed(15);
-    this.vy = 0;
+    this.x = nextRandomInteger(canvasWidth) - 100;
+    this.y = 0 - canvasHeight + 20;
+    this.vx = 0;
+    this.vy = randomSpeed(15);
     var fSize = randomSize();
 
     //문서 객체를 생성
@@ -77,19 +77,19 @@ function MovingText() {
 
 MovingText.prototype.move = function () {
     //범위검사
-    if (this.x > canvasWidth) {
-        this.vx *= -1; 
-    } else if (this.x < 0 - canvasWidth) {
-        this.x = W;
-        this.y = nextRandomInteger(canvasHeight);
+    if (this.y < 0 - canvasHeight) {
+        this.vy *= -1;
+    } else if (this.y > canvasHeight) {
+        this.x = nextRandomInteger(canvasWidth) - 100;
+        this.y = 0 - canvasHeight + 20;
         this.body.innerHTML = randomLetter();
     }
     if (this.y > H - this.height) {
         this.y = this.y - this.height;
     }
     //이동
-    this.x += this.over ? this.vx * 0.05 : this.vx;
-    this.y += this.vy;
+    this.y += this.over ? this.vy * 0.05 : this.vy;
+    this.x += this.vx;
     //화면에 이동 표시
     this.body.style.left = this.x + 'px';
     this.body.style.top = this.y + 'px';
--- js/evt2.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('h1').hover(function () {
-		$(this).css('fontSize', '120px');
-	}, function () {
-		$(this).css('fontSize', '' + fSize + ' px');
-	});
-});
